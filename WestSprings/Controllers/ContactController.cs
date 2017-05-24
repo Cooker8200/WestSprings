b55b8b1309ed10e04e2076caf872335fd5a3550c
Matthew Cook <Matthew Cook>
2017-05-24 13:25:51 -0500

    initial commit

--- WestSprings/Controllers/ContactController.cs
+++ WestSprings/Controllers/ContactController.cs
@@ -18,38 +18,49 @@ namespace WestSprings.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> ContactForm(WSContact model)
-        //{
+        public ActionResult NotSent()
+        {
+            return View();
+        }
 
-            //if (ModelState.IsValid)
-            //{
-                //var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
-                //var message = new MailMessage();
-                //var sendto = model.Sendto;
-                //message.To.Add(new MailAddress(sendto));  //change before live
-                //message.From = new MailAddress("cooker8200@hotmail.com");  //change before live
-                //message.Subject = "West Springs Contact";
-                //message.Body = string.Format(body, model.Email, model.Name, model.Message);
-                //message.IsBodyHtml = true;
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ContactForm(WSContact model)
+        {
 
-                //using (var smtp = new SmtpClient())
-                //{
-                    //var credential = new NetworkCredential
-                    //{
-                        //UserName = "Cooker8200@hotmail.com", //delete before live
-                        //Password = "MATTcook82189804!" //delete before live
-                    //};
-                    //smtp.Credentials = credential;
-                    //smtp.Host = "smtp-mail.outlook.com";  //change to correct value
-                    //smtp.Port = 587;
-                    //smtp.EnableSsl = true;
-                    //await smtp.SendMailAsync(message);
-                    //return RedirectToAction("Sent");
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                var sendto = model.Sendto;
+                message.To.Add(new MailAddress(" "));  //change before live
+                message.From = new MailAddress("cooker8200@hotmail.com");  //change before live
+                message.Subject = "West Springs Contact";
+                message.Body = string.Format(body, model.Email, model.Name, model.Message);
+                message.IsBodyHtml = true;
 
-                //}
-            //}
-        //}
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "Cooker8200@hotmail.com", //delete before live
+                        Password = "MATTcook82189804!" //delete before live
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";  //change to correct value
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+
+                }
+
+            }
+
+            else
+            {
+                return RedirectToAction("NotSent");
+            }
+        }
     }
 }
