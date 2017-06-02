bdf50ed7a3cfc766f40aea78fa91e0fc6eacd7f5
Matthew Cook <Matthew Cook>
2017-06-02 14:20:03 -0500

    sql database live

--- WestSprings/Models/IdentityModels.cs
+++ WestSprings/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace WestSprings.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("WestSpringsConnection", throwIfV1Schema: false)
         {
         }
 
