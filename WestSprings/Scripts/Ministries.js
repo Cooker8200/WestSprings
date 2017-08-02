5cce959170e41b5f8b579066b491704a46830a20
Matt Cook <Cooker8200@hotmail.com>
2017-08-01 21:28:12 -0500

    near deployment ready.  need to code in authentication for database functions, and fix slideshow functionality.  may possibly rewrite in tier function.

---
+++ WestSprings/Scripts/Ministries.js
@@ -0,0 +1,7 @@
+﻿console.log("Ministries Script");
+
+$(document).ready(function () {
+    $("#ministries_kids").click(function () {
+        window.scroll(0, findPos(document.getElementById('kids')));
+    })
+})
